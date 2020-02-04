209dc3cae38c98c369b7a527e3530ec79b03ca31
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-04 18:36:39 +0800

    Convert closest to get all matches

--- src/closest/index.js
+++ src/closest/index.js
@@ -2,10 +2,11 @@ import $ from "../constructor";
 
 $.prototype.closest = function (selector) {
   if (!selector) return this;
-  let node = this.first();
-  do {
-    if (node.matches(selector)) {
-      return $(node);
-    }
-  } while ((node = node.parentNode) && node !== document);
+  return $(this.nodes.map(node => {
+    do {
+      if (node.matches(selector)) {
+        return node;
+      }
+    } while ((node = node.parentNode) && node !== document);
+  }))
 }
