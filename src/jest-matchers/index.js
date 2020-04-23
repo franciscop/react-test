d405ccfba33295e577f0503f2d3b29950e78bd41
Andrew Park <parkandrewkh@gmail.com>
2020-04-23 11:28:10 -0700

    Implements .toMatchSelector() with tests and docs

--- src/jest-matchers/index.js
+++ src/jest-matchers/index.js
@@ -1,9 +1,11 @@
 import toBeEnabled from "./toBeEnabled";
 import toHaveClass from "./toHaveClass";
 import toHaveText from "./toHaveText";
+import toMatchSelector from "./toMatchSelector";
 
 expect.extend({
   toBeEnabled,
   toHaveClass,
-  toHaveText
+  toHaveText,
+  toMatchSelector,
 });
