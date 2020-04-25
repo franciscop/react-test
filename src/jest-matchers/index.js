59673f0f8e63eb3a1e084da7066c44f98423a7cd
dreewp <parkandrewkh@gmail.com>
2020-04-25 11:13:54 -0700

    Implements .toMatchSelector() with tests and docs (#30)
    * Implements .toMatchSelector() with tests and docs
    
    * Update docs and remove comments

--- src/jest-matchers/index.js
+++ src/jest-matchers/index.js
@@ -2,10 +2,12 @@ import toBeEnabled from "./toBeEnabled";
 import toHaveAttribute from "./toHaveAttribute";
 import toHaveClass from "./toHaveClass";
 import toHaveText from "./toHaveText";
+import toMatchSelector from "./toMatchSelector";
 
 expect.extend({
   toBeEnabled,
   toHaveAttribute,
   toHaveClass,
   toHaveText,
+  toMatchSelector,
 });
---
+++ src/jest-matchers/toMatchSelector/index.js
@@ -0,0 +1,26 @@
+import { normalize, getPlainTag } from '../../helpers';
+
+export default function (frag, selectorStr) {
+  // To avoid double negations ¯\_(ツ)_/¯
+  this.affirmative = !this.isNot;
+
+  // Convert it into a plain array of nodes
+  frag = normalize(frag);
+
+  for (let el of frag) {
+    const base = getPlainTag(el);
+    const matches = el.matches(selectorStr);
+
+    if (this.affirmative && !matches) {
+      const msg = `Expected ${base} to match selector, ${selectorStr}`;
+      return { pass: false, message: () => msg };
+    }
+
+    if (this.isNot && matches) {
+      const msg = `Expected ${base} not to match selector, ${selectorStr}`;
+      return { pass: true, message: () => msg };
+    }
+  }
+
+  return { pass: !this.isNot };
+}
