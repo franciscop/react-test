c1ddf96b36a8f95d5a5c8f87a8b4d8095f6b37e4
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-11-15 15:37:01 -0800

    Create filter tests

---
+++ src/methods/filter/test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import $ from '../../';
+import 'babel-polyfill';
+
+const $list = $(
+  <ul>
+    <li className="foo">A</li>
+    <li className="bar">B</li>
+    <li className="foo">
+      <a href="#" className="bar">
+        Link
+      </a>
+    </li>
+  </ul>
+);
+
+describe('.filter()', () => {
+  it('returns only nodes which matched the selector', async () => {
+    expect($list.find('li').filter('.foo')).toHaveLength(2);
+  });
+
+  it('only checks current nodes, not their children', async () => {
+    expect($list.find('li').filter('.bar')).toHaveLength(1);
+  });
+
+  it('returns null if no matching nodes', async () => {
+    expect($list.find('li').filter('.baz')).toHaveLength(0);
+  });
+});
