eb61b4f80c50de82638560579f816b9ebc5aab7b
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-12-03 17:42:26 -0800

    Update tests

--- src/methods/parent/test.js
+++ src/methods/parent/test.js
@@ -3,10 +3,10 @@ import $ from '../../';
 import 'babel-polyfill';
 
 const $list = $(
-  <ul className="baz">
+  <ul className="boo">
     <li className="foo">A</li>
     <li className="bar">
-      <a href="#" className="bar">
+      <a href="#" className="baz">
         Link 1
       </a>
     </li>
@@ -20,14 +20,18 @@ const $list = $(
 
 describe('.parent()', () => {
   it('returns only the direct parent of a single node', async () => {
-    expect($list.find('li').parent()).toHaveClass('baz');
+    expect($list.find('.baz').parent().nodes).toHaveLength(1);
+  });
+
+  it('returns only one parent if given multiple children of the same parent', async () => {
+    expect($list.find('li').parent().nodes).toHaveLength(1);
   });
 
   it('returns multiple parents of multiple nodes', async () => {
     expect($list.find('a').parent().nodes).toHaveLength(2);
   });
 
-  it('returns empty if no parent node', async () => {
-    expect($list.find('li').parent()).toHaveClass('baz');
+  it('returns empty if no parent', async () => {
+    expect($list.find('.boo').parent().nodes).toHaveLength(0);
   });
 });
