3be23722ab5789ddef01d4397f3cd9bf367aac96
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-12-03 16:11:23 -0800

    Implement parent method

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import "array-flat-polyfill";
-import "./jest-matchers";
+import 'array-flat-polyfill';
+import './jest-matchers';
 
-import $ from "./methods/constructor";
+import $ from './methods/constructor';
 
 // Import all of the methods which will modify the $.prototype straight away
-import "./methods/attr";
-import "./methods/click";
-import "./methods/data";
-import "./methods/find";
-import "./methods/get";
-import "./methods/first";
-import "./methods/html";
-import "./methods/is";
-import "./methods/last";
-import "./methods/unique";
-import "./methods/map";
-import "./methods/submit";
-import "./methods/text";
-import "./methods/toArray";
-import "./methods/trigger";
-import "./methods/type";
-import "./methods/children";
-import "./methods/closest";
+import './methods/attr';
+import './methods/click';
+import './methods/data';
+import './methods/find';
+import './methods/get';
+import './methods/first';
+import './methods/html';
+import './methods/is';
+import './methods/last';
+import './methods/unique';
+import './methods/map';
+import './methods/parent';
+import './methods/submit';
+import './methods/text';
+import './methods/toArray';
+import './methods/trigger';
+import './methods/type';
+import './methods/children';
+import './methods/closest';
 
-export { default as until } from "./helpers/until";
+export { default as until } from './helpers/until';
 
 // Export the whole thing
 export default $;
---
+++ src/methods/parent/index.js
@@ -0,0 +1,5 @@
+import $ from '../constructor';
+
+$.prototype.parent = function () {
+  return this.map((node) => node.parentNode);
+};
