b74a4620077cec43b42adfc8b7d8a21cd50e52c8
Francisco <public@francisco.io>
2026-04-05 01:30:54 +0900

    Fixed jest types

--- src/jest-matchers/index.ts
+++ src/jest-matchers/index.ts
@@ -3,10 +3,10 @@ import toHaveAttribute from "./toHaveAttribute/index.ts";
 import toHaveClass from "./toHaveClass/index.ts";
 import toHaveError from "./toHaveError/index.ts";
 import toHaveHtml from "./toHaveHtml/index.ts";
+import toHaveStyle from "./toHaveStyle/index.ts";
 import toHaveText from "./toHaveText/index.ts";
 import toHaveValue from "./toHaveValue/index.ts";
 import toMatchSelector from "./toMatchSelector/index.ts";
-import toHaveStyle from "./toHaveStyle/index.ts";
 
 expect.extend({
   toBeEnabled,
@@ -19,6 +19,22 @@ expect.extend({
   toMatchSelector,
   toHaveStyle,
 });
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeEnabled(): R;
+      toHaveAttribute(attr: string, val?: string | RegExp | boolean): R;
+      toHaveClass(...classes: (string | string[])[]): R;
+      toHaveError(message?: string): R;
+      toHaveHtml(html: string): R;
+      toHaveStyle(styles: string | Record<string, unknown>): R;
+      toHaveText(text: string): R;
+      toHaveValue(value?: string | boolean | number): R;
+      toMatchSelector(selector: string): R;
+    }
+  }
+}
 
 declare module "vitest" {
   interface Matchers<T = any> {
