37670dbdb2c90afeef6a883c413c229f80c7a8a1
Francisco <public@francisco.io>
2026-04-11 10:05:34 +0900

    Added regex to .toHaveError() in vitest

--- index.d.ts
+++ index.d.ts
@@ -63,32 +63,24 @@ export function until(arg?: (() => unknown) | object): any;
 export { $ as default };
 
 export {};
+interface ReactTestMatchers<R> {
+  toBeEnabled(): R;
+  toHaveAttribute(attr: string, val?: string | RegExp | boolean): R;
+  toHaveClass(...classes: (string | string[])[]): R;
+  toHaveError(message?: string | RegExp): R;
+  toHaveHtml(html: string): R;
+  toHaveStyle(styles: string | Record<string, unknown>): R;
+  toHaveText(text: string): R;
+  toHaveValue(value?: string | boolean | number): R;
+  toMatchSelector(selector: string): R;
+}
+
 declare global {
   namespace jest {
-    interface Matchers<R> {
-      toBeEnabled(): R;
-      toHaveAttribute(attr: string, val?: string | RegExp | boolean): R;
-      toHaveClass(...classes: (string | string[])[]): R;
-      toHaveError(message?: string | RegExp): R;
-      toHaveHtml(html: string): R;
-      toHaveStyle(styles: string | Record<string, unknown>): R;
-      toHaveText(text: string): R;
-      toHaveValue(value?: string | boolean | number): R;
-      toMatchSelector(selector: string): R;
-    }
+    interface Matchers<R> extends ReactTestMatchers<R> {}
   }
 }
 
 declare module "vitest" {
-  interface Matchers<T = any> {
-    toBeEnabled(): T;
-    toHaveAttribute(attr: string, val?: string | RegExp | boolean): T;
-    toHaveClass(...classes: (string | string[])[]): T;
-    toHaveError(message?: string): T;
-    toHaveHtml(html: string): T;
-    toHaveStyle(styles: string | Record<string, unknown>): T;
-    toHaveText(text: string): T;
-    toHaveValue(value?: string | boolean | number): T;
-    toMatchSelector(selector: string): T;
-  }
+  interface Matchers<T = any> extends ReactTestMatchers<T> {}
 }
